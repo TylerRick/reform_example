2ce82c3ab1b04bfb45123211cfc7406bd27a52d7
Tyler Rick <tyler.rick@k3integrations.com>
2014-03-25 07:29:25 -0700

    Use before_action callbacks to simplify AlbumsController actions

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,53 +1,57 @@
 class AlbumsController < ApplicationController
 
+  before_action :find_album,  only: [:show, :edit, :update]
+  before_action :build_album, only: [:new, :create]
+  before_action :build_form,  only: [:new, :create, :edit, :update]
+
   def index
     @albums = Album.all
   end
 
   def show
-    @album = Album.find(params[:id])
   end
 
   def new
-    # Build a random amount of songs
-    album = Album.new
-    (1+rand(7)).ceil.times do
-      album.songs.build
-    end
-    @form = Forms::AlbumForm.new(album)
   end
 
   def edit
-    album = Album.find(params[:id])
-    @form = Forms::AlbumForm.new(album)
   end
 
   def create
-    album = Album.new
-    # Build a Song object for each song that was submitted from the form
-    params[:album][:songs_attributes].size.times do
-      album.songs.build
-    end
-    @form = Forms::AlbumForm.new(album)
-
-    if @form.validate(params["album"])
+    if @form.validate(params[:album])
       @form.save
-      redirect_to album_path(album)
+      redirect_to album_path(@album)
     else
       render :new
     end
   end
 
   def update
-    album = Album.find(params[:id])
-    @form = Forms::AlbumForm.new(album)
-
-    if @form.validate(params["album"])
+    if @form.validate(params[:album])
       @form.save
-      redirect_to album_path(album)
+      redirect_to album_path(@album)
     else
       render :edit
     end
+  end
+
+private
+
+  def find_album
+    @album = Album.find(params[:id])
+  end
+
+  def build_album
+    @album = Album.new
+    # Build a Song object for each song that was submitted from the form
+    count = params[:album] ? params[:album][:songs_attributes].size : (1+rand(7)).ceil
+    count.times do
+      @album.songs.build
+    end
+  end
+
+  def build_form
+    @form = Forms::AlbumForm.new(@album)
   end
 
 end
